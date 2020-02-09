63f1a4750e2aad900421986f7f87b2a947d3ac39
Hadi <h.elmougy@gmail.com>
2020-02-09 13:44:52 +0100

    remove registry and admin

--- src/main/java/io/github/shield/ShieldAdmin.java
+++
@@ -1,9 +0,0 @@
-package io.github.shield;
-
-import java.util.Set;
-
-public interface ShieldAdmin {
-
-    Set<Connector> getAllConnectors();
-
-}
--- src/main/java/io/github/shield/ShieldFactory.java
+++ src/main/java/io/github/shield/ShieldFactory.java
@@ -1,6 +1,5 @@
 package io.github.shield;
 
-import io.github.shield.internal.ComponentRegistry;
 import io.github.shield.proxy.ProxyFactory;
 
 
@@ -20,7 +19,6 @@ public interface ShieldFactory {
     default  <T> T as(Class<T> type, Object... args) {
         Connector connector = connector();
         T component         = ProxyFactory.proxy(type, connector, args);
-        ComponentRegistry.INSTANCE.register(component, connector);
         return component;
     }
 
