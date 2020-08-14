7d12bc3729c9ec891f50af6c3b2facf85900c4f5
Hadi <h.elmougy@gmail.com>
2020-08-14 14:27:36 +0000

    measurement params

--- src/main/java/io/github/shield/metrics/Measured.java
+++ src/main/java/io/github/shield/metrics/Measured.java
@@ -1,5 +1,14 @@
 package io.github.shield.metrics;
 
 public interface Measured {
+    int arrivalCount();
+    double arrivalRate();
+    int serviceRate();
+    double throughput();
+    double avgProcessingTime();
+    int waitCount();
+    double avgWaitTime();
+    int inProcessingCount();
+    int currentTotalCount();
     
 }
